1d93f1083e36949c9811da69652d5507eaa94963
Leifeng2000 <leifeng7112@gmail.com>
2024-04-16 19:00:30 +0700

    Update

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Add();
+    }
+
+
+    static void Add()
+    {
+        Console.WriteLine("Truong caua ban la " + SinhVien.School);
+        SinhVien sv = new SinhVien();
+        Console.WriteLine("Lop cua ban la "+ sv.MyClass);
+    }
+}
+class SinhVien
+{
+    public string MyClass="CNTT";
+    public static string School="Fpt";
+}
